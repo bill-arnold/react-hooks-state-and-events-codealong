15cf0886d33d66e42cf783a6eb813f2784336ccd
bill <bill.arnold@student.moringaschool.com>
2023-10-29 14:05:16 +0300

    complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,39 @@
-import React from "react";
+// import React from "react";
+
+// function Toggle() {
+//   return <button>OFF</button>;
+// }
+
+// export default Toggle;
+
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  
+  const [isOn, setIsOn] = useState(false);
+
+  
+  function handleClick() {
+    setIsOn((prevIsOn) => !prevIsOn);
+  }
+
+
+  const buttonStyle = {
+    background: isOn ? "red" : "white",
+    padding: "10px",
+    margin: "10px",
+    fontSize: "16px",
+    cursor: "pointer",
+  };
+
+  return (
+    <div>
+      <h2>Toggle Component</h2>
+      <button style={buttonStyle} onClick={handleClick}>
+        {isOn ? "ON" : "OFF"}
+      </button>
+    </div>
+  );
 }
 
 export default Toggle;
